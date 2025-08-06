ca66ce254ccdfea7eeeeae767e53596b254cc9b2
wokieo <Cottvell@outlook.com>
2025-08-07 07:28:28 +0800

    Create fox_taro.mk

---
+++ fox_taro.mk
@@ -0,0 +1,35 @@
+#
+#	This file is part of the OrangeFox Recovery Project
+# 	Copyright (C) 2023 The OrangeFox Recovery Project
+#
+#	OrangeFox is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+#	any later version.
+#
+#	OrangeFox is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
+#
+# 	This software is released under GPL version 3 or any later version.
+#	See <http://www.gnu.org/licenses/>.
+#
+# 	Please maintain this if you use this script or any part of it
+#
+
+# OrangeFox settings
+OF_HIDE_NOTCH := 1
+OF_USE_GREEN_LED := 0
+OF_FLASHLIGHT_ENABLE := 1
+OF_ALLOW_DISABLE_NAVBAR := 0
+OF_ENABLE_USB_STORAGE := 1
+
+# OrangeFox GUI settings
+OF_SCREEN_H := 2412
+OF_STATUS_H := 100
+OF_STATUS_INDENT_LEFT := 48
+OF_STATUS_INDENT_RIGHT := 48
+OF_HIDE_NOTCH := 1
+OF_CLOCK_POS := 1
+OF_OPTIONS_LIST_NUM := 9
